31fc1c5291dab1940a803e75a754b1a26e00a2b7
Brendan Hazama <brenhazama@gmail.com>
2017-08-14 13:13:46 -1000

    FIRST

--- functions.js
+++ functions.js
@@ -74,7 +74,10 @@
  * @param {number} x, number to be squared
  * @return {number} squared
  */
-
+ function squared(num){
+  return num * num;
+ }
+ console.log(squared(3)); 
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -84,6 +87,32 @@
  * @param {number} y
  * @return {number} the result
  */
+ function equation(operation, num1, num2){
+  var result;
+  switch(operation) {
+  case "add" :
+    result = num1 + num2;
+    console.log(num1 + " + " + num2 + " = " + result);
+    break;
+  case "subtract" :
+    result = num1 - num2;
+    console.log(num1 + " - " + num2 +" = " + result);
+    break;
+  case "multiply" :
+    result = num1 * num2;
+    console.log(num1 + " * "  + num2 + " = " + result);
+    break;
+  case "divide" :
+    result = num1 / num2;
+    console.log(num1 + " / " + num2 + " = " + result);
+    break;
+  default :
+    result = 0;
+    console.log("This is not a valid operator. Please try again.");
+  }
+ }
+ equation("divide", 2, 3);
+
 
 
 /**
@@ -92,23 +121,42 @@
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
-
-
+ function isGreaterThan(num1,num2){
+  if (num1 > num2){
+    return true;
+  }else {
+    return false;
+  }
+ }
+ console.log(isGreaterThan(1,5));
 /**
  * Returns true if `a` is less than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
-
-
+ function isLessThan(num1, num2){
+  if (num1 < num2){
+    return true;
+  }else {
+    return false;
+  }
+ }
+ console.log(isLessThan(3,4));
 /**
  * Returns true if `a` and `b` are equal.
  * @param {number} a
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
-
+  function isEqualTo(num1, num2){
+    if (num1 === num2){
+      return true;
+    }else {
+      return false;
+    }
+  }
+  console.log(isEqualTo(3,3));
 
 /**
  * Returns the smallest value of two numbers.
@@ -116,30 +164,65 @@
  * @param {number} y
  * @return {number} the smallest number
  */
+ function min(num1, num2){
+  return Math.min(num1,num2);
+ }
+ console.log(min(33,45));
 
-
+ 
+ function min1(num1, num2){
+  if (num1 < num2){
+    return num1;
+  }else{
+    return num2;
+  }
+ }
+ console.log(min1(33,45));
 /**
  * Returns the largest value of two numbers.
  * @param {number} x
  * @param {number} y
  * @return {number} the largest number
  */
+ function max(num1,num2){
+  return Math.max(num1,num2);
+ }
+ console.log(max(22,44));
 
-
+ function max1(num1, num2){
+  if(num1 > num2){
+    return num1;
+  }else{
+    return num2;
+  }
+ }
+ console.log(max1(33,44));
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
-
-
+ function isEven(num){
+  if(num % 2 === 0){
+    return true;
+  }else {
+    return false;
+  }
+ }
+ console.log(isEven(66));
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
-
-
+ function isOdd(num){
+  if (num % 2 ===1){
+    return true;
+  }else {
+    return false;
+  }
+ }
+ console.log(isOdd(12));
 /**
  * Returns a letter grade.
  * "A": 90-100%
@@ -151,8 +234,21 @@
  * @param {number} total maximum possible score
  * @return {string} the score represented as a letter grade
  */
-
-
+ function letterGrader(score, max){
+  var percentage = score / max * 100;
+  if (percentage >= 90){
+    return "A";
+  }else if (percentage < 90 && percentage >= 80) {
+    return "B";
+  }else if (percentage < 80 && percentage >= 70) {
+    return "C";
+  }else if (percentage < 70 && percentage >= 60) {
+    return "D";
+  }else {
+    return "F";
+  }
+ }
+ console.log(letterGrader(75,100));
 /**
  * Checks if a `restaurant` object has a `reviews` property.
  * If it does, increase the property's `reviews` value by 1.
@@ -160,15 +256,30 @@
  * @param {object} restaurant   represents a restaurant object
  * @return {object} restaurant
  */
+ var thing = {
+  reviews : 4,
+  stars : 3
+ };
 
-
+ function propCheck(obj){
+  if (obj.hasOwnProperty("reviews")){
+    obj.reviews += 1;
+  }else {
+    obj.reviews = 1;
+  }
+  return obj;
+  }
+  console.log(propCheck(thing));
 /**
  * Joins two strings with a space.
  * @param {string} word1
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
-
+ function compound(word1, word2){
+  return word1 + " " + word2;
+ }
+ console.log(compound("honda", "civic"));
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
@@ -177,4 +288,11 @@
  * @param {number} radius
  * @return {object} circle
  */
-
+ function circleObject(radius){
+  var circle = {
+    circumference: 2 * Math.PI * radius,
+    area: Math.PI * (radius * radius)
+  };
+  return circle;
+ }
+console.log(circleObject(4));
